5b11d59c7e17eea025208b9284c396078bb49995
FrankOcrospoma <frankocrospomaugaz@gmail.com>
2024-06-05 01:54:53 -0500

    yea

---
+++ App/message.py
@@ -0,0 +1,40 @@
+import pywhatkit
+from datetime import datetime
+import time
+import pyautogui
+import pygetwindow as gw
+
+def send_whatsapp_message(phone_number, message):
+    # Obtener todas las ventanas abiertas
+    windows = gw.getAllTitles()
+
+    # Verificar si la ventana de WhatsApp Web ya está abierta
+    whatsapp_window = None
+    for window in windows:
+        print(window)
+        if 'WhatsApp' in window:
+            whatsapp_window = window
+            break
+
+    # Si no hay una ventana de WhatsApp Web abierta, usar pywhatkit para abrir una
+    if not whatsapp_window:
+        seconds = time.time() + 40
+        date = datetime.fromtimestamp(seconds)
+        pywhatkit.sendwhatmsg(phone_number, message, date.hour, date.minute)
+        time.sleep(15)  # Tiempo para abrir la ventana de WhatsApp Web
+        pyautogui.press("enter")  # Pulsar "Enter" para abrir el chat
+    else:
+        # Seleccionar la ventana de WhatsApp Web existente
+        gw.getWindowsWithTitle(whatsapp_window)[0].activate()
+        print("activate")
+
+        # Abrir el chat con el número especificado
+        url = f"https://web.whatsapp.com/send?phone={phone_number}"
+        pywhatkit.open_web(url)
+        time.sleep(10)  # Tiempo para cargar el chat con el número especificado
+        pyautogui.typewrite(message)
+        pyautogui.press("enter")
+
+# Uso de la función
+send_whatsapp_message("+51916702954", "kbro4")
+print("funciono")
